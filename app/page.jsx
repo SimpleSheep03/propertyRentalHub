d97cdd6f79ad85e5b7fee84c2a6fe5ab300ff9c6
Sambhav Sinha <sambhavsinha38@gmail.com>
2024-05-17 12:06:00 +0530

    implement hero section  , loader , not found and render properties

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Hero from '@/components/Hero'
+import InfoBoxes from '@/components/InfoBoxes'
+import HomeProperties from '@/components/HomeProperties'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>
+      <Hero/>
+      <InfoBoxes/>
+      <HomeProperties/>
+    </div>
   )
 }
 
