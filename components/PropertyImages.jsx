0cc218afa2bfe91a7c1efc20532ca19ef7842bf9
Sambhav Sinha <sambhavsinha38@gmail.com>
2024-05-18 19:07:27 +0530

    implement profile

--- components/PropertyImages.jsx
+++ components/PropertyImages.jsx
@@ -11,7 +11,7 @@ const PropertyImages = ({ images }) => {
                 <div className='grid grid-cols-2 gap-4'>
                     {images.map((image , index) => (
                         <div key={index} className={`${images.length == 3 && index == 2 ? 'col-span-2' : 'col-span-1'}`}>
-                            <Image src={images[0]} alt='' className='object-cover h-[400px] mx-auto w-full' width={1800} height={400} sizes='100vw' priority={true}/>
+                            <Image src={image} alt='' className='object-cover h-[400px] mx-auto w-full' width={1800} height={400} sizes='100vw' priority={true}/>
                         </div>
                     ))}
 
